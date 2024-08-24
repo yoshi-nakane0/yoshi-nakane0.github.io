2c9aab5974f567feb775cd55be0eb9a2461e8385
yoshi-nakane0 <yos242012@yahoo.co.jp>
2024-08-24 12:55:58 +0900

    upload

--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -1,14 +1,51 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+// News.tsx
+import React, { useState, useEffect } from 'react';
+import '../styles/news.css';
+import newsData from '../context/news.json'; // news.jsonをimport
 
-function NewsPage() {
-  return (
-    <div>
-      <h1>Second Page</h1>
-      <p>This is the second page of our application.</p>
-      <Link to="/">Back to Home</Link>
-    </div>
-  );
+interface NewsItem { 
+  title: string; 
+  content: string; 
 }
 
-export default NewsPage;
+const News = () => {
+  const [newsItems, setNewsItems] = useState<NewsItem[]>([]); // 型を明示的に指定
+  const [selectedNews, setSelectedNews] = useState<string | null>(null);
+
+  useEffect(() => {
+    setNewsItems(newsData); 
+  }, []);
+
+  const handleClick = (content: string) => {
+    setSelectedNews(content);
+  };
+
+  return (
+    <div className="news-container">
+      <div className="status-bar">
+        <button className="status-bar-item" onClick={() => { window.location.href = '/'; }}>
+          ホーム
+        </button>
+      </div>
+      <h1 className="news-heading">ニュース</h1>
+
+      <ul className="news-list">
+        {newsItems.map((item: { title: string; content: string }, index: number) => (
+          <li key={index} className="news-item">
+            <button onClick={() => handleClick(item.content)} className="news-link">
+              <span className="news-title">{item.title}</span>
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      {selectedNews && (
+        <div className="news-content">
+          <p dangerouslySetInnerHTML={{ __html: selectedNews.replace(/\n/g, '<br />') }} /> 
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default News;
