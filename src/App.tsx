476493f989090ea905b6ccb316f5208f7807db63
yoshi-nakane0 <yos242012@yahoo.co.jp>
2024-10-03 22:35:37 +0900

    upload

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import Home from './pages/Home.tsx';
 import Schedule from './pages/Schedule.tsx';
 import News from './pages/News.tsx';
 import Earning from './pages/Earning.tsx';
+import Target from './pages/Target.tsx';
+import Control from './pages/Control.tsx';
+import Trending from './pages/Trending.tsx';
 function App() {
   return (
     <Router>
@@ -12,6 +15,9 @@ function App() {
         <Route path="/schedule" element={<Schedule />} />
         <Route path="/news" element={<News />} />
         <Route path="/earning" element={<Earning />} />
+        <Route path="/target" element={<Target />} />
+        <Route path="/control" element={<Control />} />
+        <Route path="/trending" element={<Trending />} />
       </Routes>
     </Router>
   );
